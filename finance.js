db99c9fc2091e480f0140ce51cca08f636921380
velasquezMX <velasdemongrinder@gmail.com>
2014-02-01 20:14:03 -0800

    added touch drag capabilities

--- finance.js
+++ finance.js
@@ -3,13 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-$( document ).ready(function() {
- $("img").init();
- 
-});
-
-
 var loan = new Loan();
+
 
 function Loan(){
     this.PV;
@@ -29,27 +24,6 @@ function clearContent(element){
     element.value = '';
 }
 
-function touchHandler(event) {
-    var touch = event.changedTouches[0];
-
-    var simulatedEvent = document.createEvent("MouseEvent");
-        simulatedEvent.initMouseEvent({
-        touchstart: "mousedown",
-        touchmove: "mousemove",
-        touchend: "mouseup"
-    }[event.type], true, true, window, 1,
-        touch.screenX, touch.screenY,
-        touch.clientX, touch.clientY, false,
-        false, false, false, 0, null);
-
-    touch.target.dispatchEvent(simulatedEvent);
-    event.preventDefault();
-}
-
-function init() {
-    document.addEventListener("touchstart", touchHandler, true);
-    document.addEventListener("touchmove", touchHandler, true);
-    document.addEventListener("touchend", touchHandler, true);
-    document.addEventListener("touchcancel", touchHandler, true);
-}
-
+$(function() {
+        $( ".truck" ).draggable();
+    });
